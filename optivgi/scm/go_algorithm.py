258dc7eab50a761189c8ac014261a2d7c1d5afb7
Nithin Manne <nmanne@anl.gov>
2025-04-23 17:09:59 -0500

    Update license information and bump version to 0.1.9

--- optivgi/scm/go_algorithm.py
+++ optivgi/scm/go_algorithm.py
@@ -1,3 +1,18 @@
+# Copyright 2025 UChicago Argonne, LLC All right reserved
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://github.com/argonne-vci/Opti-VGI/blob/main/LICENSE
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# pylint: disable=missing-function-docstring
 """
 Provides a custom heuristic-based SCM algorithm ("GoAlgorithm").
 
@@ -6,7 +21,6 @@ minimum power requirements and then distributing remaining capacity based on
 fairness factors and optional strategies like front-loading power. It does not rely
 on external optimization solvers like PuLP.
 """
-# pylint: disable=missing-function-docstring
 import math
 import logging
 from dataclasses import dataclass, field
